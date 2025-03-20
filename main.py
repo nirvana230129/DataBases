6e149cfb3b15116481691992945b3ad99410b415
arif_meilanov <arif.m.04@mail.ru>
2025-03-20 22:07:52 +0300

    Cleaning part separated into clean.sql

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ conn = sqlite3.connect('movies_and_series.db')
 cursor = conn.cursor()
 
 # Чтение и выполнение SQL-скрипта
+with open('clean.sql', 'r') as file:
+    sql_script = file.read()
+    cursor.executescript(sql_script)
 with open('main.sql', 'r') as file:
     sql_script = file.read()
     cursor.executescript(sql_script)
