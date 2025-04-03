3181855d2d14954a927634cdb0c096bf765fd0a5
arif_meilanov <arif.m.04@mail.ru>
2025-04-03 18:04:27 +0300

    Task 1 is approved (scheme and architecture of database)

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import sqlite3
 
-# Подключение к базе данных
 conn = sqlite3.connect('movies_and_series.db')
 cursor = conn.cursor()
 
-# Чтение и выполнение SQL-скрипта
 with open('clean.sql', 'r') as file:
     sql_script = file.read()
     cursor.executescript(sql_script)
-with open('main2.sql', 'r') as file:
+
+with open('main.sql', 'r') as file:
     sql_script = file.read()
     cursor.executescript(sql_script)
 
-# Сохранение изменений и закрытие соединения
 conn.commit()
 conn.close()
--- main.sql
+++ main.sql
@@ -44,104 +44,68 @@ CREATE TABLE IF NOT EXISTS Roles (
     name VARCHAR UNIQUE NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS Characters (
-    id INTEGER PRIMARY KEY,
-    name VARCHAR NOT NULL,
-    description TEXT
-);
-
 CREATE TABLE IF NOT EXISTS Episodes (
     id INTEGER PRIMARY KEY,
-    tvshow_id INTEGER NOT NULL,
+    tv_show_id INTEGER NOT NULL,
     season_number INTEGER,
     episode_number INTEGER,
     title VARCHAR,
     release_date DATE,
     duration TIME,
     rating FLOAT,
-    FOREIGN KEY (tvshow_id) REFERENCES TVShows(id)
+    FOREIGN KEY (tv_show_id) REFERENCES TVShows(id)
 );
 
-CREATE TABLE IF NOT EXISTS MovieGenres (
+CREATE TABLE IF NOT EXISTS ContentGenres (
     id INTEGER PRIMARY KEY,
-    movie_id INTEGER NOT NULL,
+    movie_id INTEGER,
+    tv_show_id INTEGER,
     genre_id INTEGER NOT NULL,
-    PRIMARY KEY (movie_id, genre_id),
     FOREIGN KEY (movie_id) REFERENCES Movies(id),
-    FOREIGN KEY (genre_id) REFERENCES Genres(id)
+    FOREIGN KEY (tv_show_id) REFERENCES TVShows(id),
+    FOREIGN KEY (genre_id) REFERENCES Genres(id),
+    CHECK (
+        (movie_id IS NOT NULL AND tv_show_id IS NULL)
+        OR
+        (movie_id IS NULL AND tv_show_id IS NOT NULL)
+    )
 );
 
-CREATE TABLE IF NOT EXISTS TVShowGenres (
+CREATE TABLE IF NOT EXISTS ContentCountries (
     id INTEGER PRIMARY KEY,
-    tvshow_id INTEGER NOT NULL,
-    genre_id INTEGER NOT NULL,
-    PRIMARY KEY (tvshow_id, genre_id),
-    FOREIGN KEY (tvshow_id) REFERENCES TVShows(id),
-    FOREIGN KEY (genre_id) REFERENCES Genres(id)
-);
-
-CREATE TABLE IF NOT EXISTS MovieCountries (
-    id INTEGER PRIMARY KEY,
-    movie_id INTEGER NOT NULL,
+    movie_id INTEGER,
+    tv_show_id INTEGER,
     country_id INTEGER NOT NULL,
-    PRIMARY KEY (movie_id, country_id),
     FOREIGN KEY (movie_id) REFERENCES Movies(id),
-    FOREIGN KEY (country_id) REFERENCES Countries(id)
+    FOREIGN KEY (tv_show_id) REFERENCES TVShows(id),
+    FOREIGN KEY (country_id) REFERENCES Countries(id),
+    CHECK (
+        (movie_id IS NOT NULL AND tv_show_id IS NULL)
+        OR
+        (movie_id IS NULL AND tv_show_id IS NOT NULL)
+    )
 );
 
-CREATE TABLE IF NOT EXISTS TVShowCountries (
+CREATE TABLE IF NOT EXISTS ContentPersonnel (
     id INTEGER PRIMARY KEY,
-    tvshow_id INTEGER NOT NULL,
-    country_id INTEGER NOT NULL,
-    PRIMARY KEY (tvshow_id, country_id),
-    FOREIGN KEY (tvshow_id) REFERENCES TVShows(id),
-    FOREIGN KEY (country_id) REFERENCES Countries(id)
-);
-
-CREATE TABLE IF NOT EXISTS MoviePersonnel (
-    id INTEGER PRIMARY KEY,
-    movie_id INTEGER NOT NULL,
+    movie_id INTEGER,
+    tv_show_id INTEGER,
     person_id INTEGER NOT NULL,
     role_id INTEGER NOT NULL,
-    PRIMARY KEY (movie_id, person_id, role_id),
     FOREIGN KEY (movie_id) REFERENCES Movies(id),
+    FOREIGN KEY (tv_show_id) REFERENCES TVShows(id),
     FOREIGN KEY (person_id) REFERENCES Personnel(id),
-    FOREIGN KEY (role_id) REFERENCES Roles(id)
+    FOREIGN KEY (role_id) REFERENCES Roles(id),
+    CHECK (
+        (movie_id IS NOT NULL AND tv_show_id IS NULL)
+        OR
+        (movie_id IS NULL AND tv_show_id IS NOT NULL)
+    )
 );
 
-CREATE TABLE IF NOT EXISTS TVShowPersonnel (
+CREATE TABLE IF NOT EXISTS ContentCharacters (
     id INTEGER PRIMARY KEY,
-    tvshow_id INTEGER NOT NULL,
-    person_id INTEGER NOT NULL,
-    role_id INTEGER NOT NULL,
-    PRIMARY KEY (tvshow_id, person_id, role_id),
-    FOREIGN KEY (tvshow_id) REFERENCES TVShows(id),
-    FOREIGN KEY (person_id) REFERENCES Personnel(id),
-    FOREIGN KEY (role_id) REFERENCES Roles(id)
-);
-
-CREATE TABLE IF NOT EXISTS MovieCharacters (
-    id INTEGER PRIMARY KEY,
-    movie_id INTEGER NOT NULL,
-    character_id INTEGER NOT NULL,
-    person_id INTEGER NOT NULL,
-    role_id INTEGER NOT NULL,
-    PRIMARY KEY (movie_id, character_id, person_id, role_id),
-    FOREIGN KEY (movie_id) REFERENCES Movies(id),
-    FOREIGN KEY (character_id) REFERENCES Characters(id),
-    FOREIGN KEY (person_id) REFERENCES Personnel(id),
-    FOREIGN KEY (role_id) REFERENCES Roles(id)
-);
-
-CREATE TABLE IF NOT EXISTS TVShowCharacters (
-    id INTEGER PRIMARY KEY,
-    tvshow_id INTEGER NOT NULL,
-    character_id INTEGER NOT NULL,
-    person_id INTEGER NOT NULL,
-    role_id INTEGER NOT NULL,
-    PRIMARY KEY (tvshow_id, character_id, person_id, role_id),
-    FOREIGN KEY (tvshow_id) REFERENCES TVShows(id),
-    FOREIGN KEY (character_id) REFERENCES Characters(id),
-    FOREIGN KEY (person_id) REFERENCES Personnel(id),
-    FOREIGN KEY (role_id) REFERENCES Roles(id)
+    content_person_id INTEGER NOT NULL,
+    character_name VARCHAR NOT NULL,
+    FOREIGN KEY (content_person_id) REFERENCES ContentPersonnel(id)
 );
