f18c9fa010dcb7eab782435c7a390de2b0c781aa
arif_meilanov <arif.m.04@mail.ru>
2025-04-17 17:23:49 +0300

    SQL scripts were distributed into sql folder. ID column of most of the tables has 'autoincrement' feature. Now can fill personnel, genres, countries

--- db/main.py
+++ db/main.py
@@ -19,16 +19,16 @@ class Database:
         self._conn.close()
 
     def clean(self):
-        with open('clean.sql', 'r') as file:
+        with open('sql/clean.sql', 'r') as file:
             sql_script = file.read()
             self._cursor.executescript(sql_script)
 
     def create(self):
-        with open('create.sql', 'r') as file:
+        with open('sql/create.sql', 'r') as file:
             sql_script = file.read()
             self._cursor.executescript(sql_script)
 
-    def fill_with_personnel(self, personnel_file):
+    def _fill_personnel(self, personnel_file):
         personnel_df = pd.read_csv(personnel_file)
 
         for i, row in tqdm(personnel_df.iterrows(), total=len(personnel_df)):
@@ -41,6 +41,33 @@ class Database:
                 except Exception as e:
                     raise Exception(f'Error inserting personnel {i}, {row}:\n{e}\n' + '=' * 80)
 
+    def _fill_genres(self, roles_file):
+        genres_df = pd.read_csv(roles_file)
+        for i, row in tqdm(genres_df.iterrows(), total=len(genres_df)):
+            try:
+                self._cursor.execute('''
+                    INSERT INTO Genres (name)
+                    VALUES (?);
+                ''', (row['Genre'],))
+            except Exception as e:
+                raise Exception(f'Error inserting genre {i}, {row}:\n{e}\n' + '=' * 80)
+
+    def _fill_countries(self, countries_file):
+        countries_df = pd.read_csv(countries_file)
+        for i, row in tqdm(countries_df.iterrows(), total=len(countries_df)):
+            try:
+                self._cursor.execute('''
+                    INSERT INTO Countries (name)
+                    VALUES (?);
+                ''', (row['Country'],))
+            except Exception as e:
+                raise Exception(f'Error inserting country {i}, {row}:\n{e}\n' + '=' * 80)
+
+    def fill(self, personnel_file, genres_file, countries_file):
+        self._fill_personnel(personnel_file)
+        self._fill_genres(genres_file)
+        self._fill_countries(countries_file)
+
     def print_table(self, table):
         self._cursor.execute(f'SELECT * FROM {table}')
         return self._cursor.fetchall()
@@ -49,4 +76,8 @@ class Database:
 with Database('database') as db:
     db.clean()
     db.create()
-    db.fill_with_personnel('../data/personnel_data.csv')
+    db.fill(
+        '../data/personnel_data.csv',
+        '../data/genres_data.csv',
+        '../data/countries_data.csv'
+    )
